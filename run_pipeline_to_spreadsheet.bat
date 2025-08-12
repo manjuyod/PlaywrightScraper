449a1e65fe372d4933a896290c351e840c3cb806
Brandon Millares <manjuyod@gmail.com>
2025-08-11 23:55:07 -0700

    feat: add pipeline_to_spreadsheet

---
+++ run_pipeline_to_spreadsheet.bat
@@ -0,0 +1,15 @@
+@echo off
+echo [1/3] Running Scraper...
+uv run python -m scraper.runner --franshise_id 57
+
+echo.
+echo [2/3] Updating db...
+uv run python -m scraper.work_flows.insert_grades
+
+echo.
+echo [3/3] Uploading to sheets...
+uv run python -m scraper.work_flows.update_sheets
+
+echo.
+echo Pipeline finished successfully.
+pause
