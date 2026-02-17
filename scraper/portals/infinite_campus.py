09eedbf030f6db2b2d3a189f0912d45f7823b01f
= <=>
2026-02-17 11:51:54 -0700

    fix IC to get the most recent grades from the current semester.

--- scraper/portals/infinite_campus.py
+++ scraper/portals/infinite_campus.py
@@ -1,10 +1,5 @@
 from __future__ import annotations
-
-from typing import Any, Optional
-
-from playwright.async_api import Page
 from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception_type
-
 from . import register_portal  # helper we'll create in __init__.py
 from .base import PortalEngine, PlaywrightTimeout
 from .utils import *
@@ -72,6 +67,25 @@ class InfiniteCampus(PortalEngine):
             await self.page.get_by_role("link", name=grades_button_label).click()
             await self.page.wait_for_url(grades_url_pattern, timeout=20000)
             await self.page.wait_for_load_state("networkidle")
+
+    @staticmethod
+    def term_semester_from_today() -> int:
+        """
+        Determine current academic term semester.
+
+        Return: 1 for Fall, 2 for Spring
+        """
+        from datetime import datetime
+        now = datetime.now()
+        m, y = now.month, now.year
+        if m >= 8:  # Aug–Dec → Fall of current year
+            sem = 1
+        elif m <= 5:  # Jan–May → Spring of previous fall year
+            sem = 2
+        else:  # Jun–Jul → prep for upcoming Fall
+            sem = 1
+        return sem
+
     # ---------------------- FETCH (notifications → latest per subject) -------
     @retry(
         stop=stop_after_attempt(3),
@@ -85,19 +99,25 @@ class InfiniteCampus(PortalEngine):
         # get grades
         try:
             # 0) ensure we are on the grades page and targeting the right timeframe
-            frame_selector = "main-workspace"
             await self.nav_to_grades()
-            frame = self.page.frame(name=frame_selector)
+
+            frame_selector = "main-workspace"
+            frame = self.page.frame(frame_selector)
             # target the correct timeframe
-            current_quarter = 3
-            await self.select_current_quarter(current_quarter, frame)
+
+
+
 
             # collect grades
             table_selector = "div.collapsible-card.grades__card"
             course_selector = "h4 a"
             grades_selector = ".grading-score div"
 
-            return await grades_table_to_dict(
+            # we will parse the current quarter as well as the next quarter, and keep the grades from the most recent quarter
+            final_sem_quarter = self.term_semester_from_today() * 2 # either 4 or 2
+            await self.select_quarter(final_sem_quarter, frame)
+
+            next_q_grades = await grades_table_to_dict(
                 self.page,
                 table_selector,
                 course_selector,
@@ -105,19 +125,34 @@ class InfiniteCampus(PortalEngine):
                 frame_selector=frame_selector,
                 use_soup=False
             )
-            
-        # except Exception as e:
+
+            await self.select_quarter(final_sem_quarter - 1, frame)
+            cur_q_grades = await grades_table_to_dict(
+                self.page,
+                table_selector,
+                course_selector,
+                grades_selector,
+                frame_selector=frame_selector,
+                use_soup=False
+            )
+
+            # match the possible grades from the next quarter to the current quarter
+            for course in cur_q_grades.keys():
+                recent_grade = next_q_grades.get(course, None)
+                if recent_grade:
+                    cur_q_grades[course] = recent_grade
+
+            print(cur_q_grades)
+            return cur_q_grades
+        except Exception:
+            import traceback
+            print(traceback.print_exc())
+            raise
         finally:
             print("finished fetching")
 
     @staticmethod
-    async def select_current_quarter(current_quarter: int, frame: Frame) -> None:
-        # target_prefix = "Q" if "chandleraz" in self.page.url else "QT"
-
-        # target_quarter_tag = target_prefix + str(current_quarter)
-        prefix = ["QT", "Q", "S"]
-
-
+    async def select_quarter(current_quarter: int, frame: Frame) -> None:
         timeframe = frame.get_by_role("button", name="QT" + str(current_quarter))
         if not await exists(timeframe):
             timeframe = frame.get_by_role("button", name="Q" + str(current_quarter))
